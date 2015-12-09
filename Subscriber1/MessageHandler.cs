9c42f033128f6080bd3fafd70fa305805568b6c8
SudheeraC <sudh.chk18@gmail.com>
2015-12-09 17:01:00 +0530

    sub1

---
+++ Subscriber1/MessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Subscriber1
+{
+    class MessageHandler : IHandleMessages<One>
+    {
+        public void Handle(One message)
+        {
+           //do some processing.
+           Console.WriteLine("Subscriber1+" + message.message);
+        }
+    }
+}
---
+++ Subscriber1/MyMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Subscriber1
+{
+    class MyMessageHandler : IHandleMessages<MyFirstEvent>
+    {
+        public void Handle(MyFirstEvent message)
+        {
+           //do some processing.
+           Console.WriteLine("Subscriber1+" + message.message);
+        }
+    }
+}
